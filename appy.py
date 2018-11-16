04c06f06a3ebccd4133a0573935a2428fd695c1a
gummi989 <34032023+gummi989@users.noreply.github.com>
2018-11-16 18:58:29 +0000

    Update and rename Verkefni_7.py to appy.py

--- appy.py
+++ appy.py
@@ -11,14 +11,14 @@ def nyr():
     p = request.forms.get('pass')
     n = request.forms.get('nafn')
 
-    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2507002960', passwd='mypassword',db='2507002960_Verk7')
+    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2507002960', passwd='mypassword',db='2704982729_Verkefni7')
     cur = conn.cursor()
 
-    cur.execute("SELECT count(*) From 2507002960_Verk7.user where user=%s",(u))
+    cur.execute("SELECT count(*) From 2704982729_Verkefni7.user where user=%s",(u))
     result = cur.fetchone()
 
     if result[0] == 0:
-        cur.execute("Insert into 2507002960_Verk7.user Values(%s,%s,%s)", (u,p,n))
+        cur.execute("Insert into 2704982729_Verkefni7.user Values(%s,%s,%s)", (u,p,n))
         conn.commit()
         cur.close()
         conn.close()
@@ -31,10 +31,10 @@ def doinn():
     u = request.forms.get('user')
     p = request.forms.get('pass')
 
-    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2507002960', passwd='mypassword',db='2507002960_Verk7')
+    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2704982729', passwd='mypassword',db='2704982729_Verkefni7')
     cur = conn.cursor()
 
-    cur.execute("SELECT count(*) From 2507002960_Verk7.user where user=%s and pass=%s",(u,p))
+    cur.execute("SELECT count(*) From 2704982729_Verkefni7.user where user=%s and pass=%s",(u,p))
     result = cur.fetchone()
     print(result)
 
@@ -47,9 +47,9 @@ def doinn():
 
 @route('/members')
 def member():
-    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2507002960', passwd='mypassword',db='2507002960_Verk7')
+    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2507002960', passwd='mypassword',db='2704982729_Verkefni7')
     cur = conn.cursor()
-    cur.execute("SELECT nafn From 2507002960_Verk7.user")
+    cur.execute("SELECT nafn From 2704982729_Verkefni7.user")
     result = cur.fetchall()
     cur.close()
     output = template('Static/members', rows=result)
